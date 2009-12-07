8c6a061ed3f03912b4de9f4792bdf89579a3d2bd
Tomas Janousek <tomi@nomi.cz>
2009-12-07 16:49:46 +0000

    posledni kusy cleanupu a dokumentace k flexibleread

--- lib/XMonad/Layout/FlexibleRead.hs
+++ lib/XMonad/Layout/FlexibleRead.hs
@@ -21,7 +21,10 @@ import Language.Haskell.TH
 -- | The "flexible read" layout modifier.
 data FlexibleRead l a = FlexibleRead (l a)
 
-instance (LayoutClass l a , ReadShowTree l a) => LayoutClass (FlexibleRead l) a where
+-- | The 'LayoutClass' instance for our modifier. We cannot use 'FlexibleRead'
+-- as a normal modifier in 'ModifiedLayout' since the 'Show' and 'Read'
+-- instances of 'ModifiedLayout' can not be overlapped.
+instance (LayoutClass l a, ReadShowTree (l a)) => LayoutClass (FlexibleRead l) a where
     runLayout (Workspace i (FlexibleRead l) ms) r =
         fmap (fmap FlexibleRead) `fmap` runLayout (Workspace i l ms) r
     doLayout (FlexibleRead l) r s  = fmap (fmap FlexibleRead) `fmap` doLayout l r s
@@ -29,18 +32,31 @@ instance (LayoutClass l a , ReadShowTree l a) => LayoutClass (FlexibleRead l) a 
     handleMessage (FlexibleRead l) = fmap (fmap FlexibleRead) . handleMessage l
     description (FlexibleRead l)   = description l
 
-instance (ReadShowTree l a) => Show (FlexibleRead l a) where
+-- | A non-default 'Show' instance for 'FlexibleRead'. This is what makes the
+-- output parseable even if the layout changes.
+instance (ReadShowTree (l a)) => Show (FlexibleRead l a) where
     show (FlexibleRead l) = show $ showTree l
 
-flexibleRead :: (ReadShowTree l a) => l a -> FlexibleRead l a
+-- | Apply the 'FlexibleRead' modifier to a layout.
+flexibleRead :: (ReadShowTree (l a)) => l a -> FlexibleRead l a
 flexibleRead = FlexibleRead
 
-flexibleReadsPrec :: (ReadShowTree l a) =>
+-- | A helper function for the implementation of 'Read' instance. This is more
+-- or less a reverse of 'show' that additionally applies the tree correction
+-- algorithm.
+flexibleReadsPrec :: (ReadShowTree (l a)) =>
     FlexibleRead l a -> Int -> String -> [(FlexibleRead l a, [Char])]
 flexibleReadsPrec (FlexibleRead l) i s | [(s', "")] <- readsPrec i s =
     [(FlexibleRead $ readTreeDef l $ matchTreesFst (showTree l) s', "")]
 flexibleReadsPrec _ _ _ = []
 
+-- | Given a name of your layout variable, this defines the 'Read' instance
+-- for your particular layout type and with your particular layout as a
+-- default for non-readable parts of the input.
+--
+-- Usage:
+-- > {-# LANGUAGE TemplateHaskell #-}
+-- > $( flexibleReadInstance 'myLayout )
 flexibleReadInstance :: Name -> Q [Dec]
 flexibleReadInstance lay = do
     (VarI _ typ _ _) <- reify lay
@@ -48,18 +64,27 @@ flexibleReadInstance lay = do
     return [ InstanceD [] (AppT (ConT (mkName "Read")) typ) inst ]
 
 
-class ReadShowTree l a where
-    readTreeDef :: l a -> Tree (String, String) -> l a
-    showTree :: l a -> Tree (String, String)
+-- | A type class for types that can be serialized to and from trees of
+-- @(String, String)@. Useful for data types that have a hierarchical
+-- structure, like 'ModifiedLayout'.
+class ReadShowTree a where
+    -- | Read a value from a tree, taking the unreadable parts from a
+    -- specified default value.
+    readTreeDef :: a -> Tree (String, String) -> a
+    -- | Show a value into a tree.
+    showTree :: a -> Tree (String, String)
 
-instance (Read (l a), LayoutClass l a) => ReadShowTree l a where
+-- | The 'ReadShowTree' instance for a generic layout. This shows into a leaf.
+instance (Read (l a), LayoutClass l a) => ReadShowTree (l a) where
     readTreeDef def (Node (_, val) []) = fromMaybe def $ maybeRead val
     readTreeDef def _ = def
 
     showTree a = Node (description a, show a) []
 
-instance (Read (m a), Show (m a), ReadShowTree l a, LayoutModifier m a) =>
-        ReadShowTree (ModifiedLayout m l) a where
+-- | The 'ReadShowTree' instance for a layout modifier. This shows into a node
+-- with a single child, the layout being modified.
+instance (Read (m a), Show (m a), ReadShowTree (l a), LayoutModifier m a) =>
+        ReadShowTree (ModifiedLayout m l a) where
 
     readTreeDef ~(ModifiedLayout mod lay) (Node (_, val) [child]) =
         ModifiedLayout (fromMaybe mod $ maybeRead val) (readTreeDef lay child)
@@ -68,7 +93,9 @@ instance (Read (m a), Show (m a), ReadShowTree l a, LayoutModifier m a) =>
     showTree (ModifiedLayout a b) = Node (head $ words a', a') [ showTree b ]
         where a' = show a
 
-instance (ReadShowTree l1 a, ReadShowTree l2 a) => ReadShowTree (NewSelect l1 l2) a where
+-- | The 'ReadShowTree' instance for the 'NewSelect' combinator. This shows
+-- into a node with as many children as layout choices in this select.
+instance (ReadShowTree (l1 a), ReadShowTree (l2 a)) => ReadShowTree (NewSelect l1 l2 a) where
     showTree (NewSelect b l1 l2) = Node ("NewSelect", "") sf
         where l1' = sel b $ showTree l1
               l2'@(Node (l2x,l2y) l2z) = showTree l2
@@ -82,7 +109,7 @@ instance (ReadShowTree l1 a, ReadShowTree l2 a) => ReadShowTree (NewSelect l1 l2
     readTreeDef ~(NewSelect _ l1 l2) (Node ("NewSelect", "") sf@(child : sfs@(child2:sfs2)))
         | (Node ("NewSelectBool", childb') [childchild]) <- child,
           childb <- read childb',
-          (Node ("NewSelectBool", _             ) [child2child]) <- child2 =
+          (Node ("NewSelectBool", _      ) [child2child]) <- child2 =
             let sel2 n@(Node ("NewSelectBool", _) x) | childb = Node ("NewSelectBool", "True") x
                 sel2 n = n
                 newl2 = if null sfs2
@@ -91,6 +118,7 @@ instance (ReadShowTree l1 a, ReadShowTree l2 a) => ReadShowTree (NewSelect l1 l2
             in NewSelect childb (readTreeDef l1 childchild) newl2
     readTreeDef def _ = def
 
+-- | A convenient 'read' that does not 'error' on failure.
 maybeRead :: (Read a) => String -> Maybe a
 maybeRead s =
     case reads s of
