3f1cfb4fbca0afeccc5514c6580c25c8d3d4d758
Tomas Janousek <tomi@nomi.cz>
2019-04-24 09:31:42 +0200

    Add /usr/local/sbin/thinkpad_battery.sh

---
+++ usr/local/sbin/thinkpad_battery.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+
+set -eu
+
+case "$1" in
+	charge)
+		echo 70 > /sys/class/power_supply/BAT0/charge_start_threshold
+		echo 70 > /sys/class/power_supply/BAT1/charge_start_threshold
+		echo 80 > /sys/class/power_supply/BAT0/charge_stop_threshold
+		echo 80 > /sys/class/power_supply/BAT1/charge_stop_threshold
+		;;
+
+	normal)
+		echo 40 > /sys/class/power_supply/BAT0/charge_start_threshold
+		echo 40 > /sys/class/power_supply/BAT1/charge_start_threshold
+		echo 80 > /sys/class/power_supply/BAT0/charge_stop_threshold
+		echo 80 > /sys/class/power_supply/BAT1/charge_stop_threshold
+		;;
+
+	full-int)
+		echo 90 > /sys/class/power_supply/BAT0/charge_start_threshold
+		echo 100 > /sys/class/power_supply/BAT0/charge_stop_threshold
+		;;
+
+	full-ext)
+		echo 90 > /sys/class/power_supply/BAT1/charge_start_threshold
+		echo 100 > /sys/class/power_supply/BAT1/charge_stop_threshold
+		;;
+esac
