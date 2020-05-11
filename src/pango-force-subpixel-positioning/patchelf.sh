1d927ba5dae98fa9f66c283ddbb8f353dce6234f
Tomas Janousek <tomi@nomi.cz>
2020-05-11 13:17:00 +0200

    src/pango-force-subpixel-positioning: Simplify the wrapper mechanism

--- src/pango-force-subpixel-positioning/patchelf.sh
+++ src/pango-force-subpixel-positioning/patchelf.sh
@@ -6,8 +6,6 @@ if [[ "$(ldd "$1")" =~ (libpango[^[:space:]]*)" => "(/[^[:space:]]*/libpango[^[:
 	soname="${BASH_REMATCH[1]}"
 	lib="${BASH_REMATCH[2]}"
 
-	patchelf --replace-needed "$soname" "orig-$soname" "$1"
-	patchelf --set-rpath "\$ORIGIN" "$1"
-	ln -sf "$lib" "$(dirname "$1")/orig-$soname"
+	patchelf --replace-needed "$soname" "$lib" "$1"
 	ln -sf "$(basename "$1")" "$(dirname "$1")/$soname"
 fi
