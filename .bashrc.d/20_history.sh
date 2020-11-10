91e620d5569776dfca191ff75b17001b46e8c066
Tomas Janousek <tomi@nomi.cz>
2020-11-10 15:05:10 +0000

    Periodically backup ~/.bash_history and lower HISTSIZE to 5000
    This shaves 25 ms off bash startup.

--- .bashrc.d/20_history.sh
+++ .bashrc.d/20_history.sh
@@ -2,14 +2,14 @@
 
 # don't put duplicate lines or lines starting with space in the history.
 # See bash(1) for more options
-HISTCONTROL=ignoreboth
+HISTCONTROL=ignoreboth:erasedups
 
 # append to the history file, don't overwrite it
 shopt -s histappend
 
 # for setting history length see HISTSIZE and HISTFILESIZE in bash(1)
-HISTSIZE=50000
-HISTFILESIZE=50000
+HISTSIZE=5000
+HISTFILESIZE=5000
 
 # disable history expansion (!)
 set +o histexpand
