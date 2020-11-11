c3d101c2bfebcbf575a0d6d9cd4729ccc0fd635f
Tomas Janousek <tomi@nomi.cz>
2020-11-11 15:28:47 +0000

    .bashrc: Fix slow loading of fzf completion.bash

--- .bashrc.d/90_fzf.sh
+++ .bashrc.d/90_fzf.sh
@@ -1,4 +1,7 @@
 #!bash
 
 source /usr/share/doc/fzf/examples/key-bindings.bash
-source /usr/share/doc/fzf/examples/completion.bash
+
+# https://github.com/junegunn/fzf/pull/2246
+#source /usr/share/doc/fzf/examples/completion.bash
+source ~/.bashrc.d/.fzf-completion.bash
