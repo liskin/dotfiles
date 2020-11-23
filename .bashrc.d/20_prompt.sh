a5db1831b37f89e00a637bcc20594a4fcf16de1d
Tomas Janousek <tomi@nomi.cz>
2020-11-23 00:28:26 +0000

    .bashrc: More atomic newline prompt
    Using $() instead of PROMPT_COMMAND makes this a bit more "atomic",
    lowering the likelihood that something (echoed input, usually) appears
    between the ↵\n and the prompt.

--- .bashrc.d/20_prompt.sh
+++ .bashrc.d/20_prompt.sh
@@ -6,25 +6,23 @@ else
 	__docker_ps1=
 fi
 
-PS1='\[\033[48;5;053m\033[01;$(( $? ? 31 : 32 ))m\][\u'${__docker_ps1}'@\h\[\033[01;36m\]$(__git_ps1 " ($(git repo-name):%s)")\[\033[01;33m\] \W]\$\[\033[00m\] '
+function __col1_ps1 {
+	[[ $MC_SID ]] && return
 
-unset __docker_ps1
-
-function __prompt_command_col1 {
 	local cur_y=
 	while read -t 0.0001 -r; do :; done
 	IFS='[;' read -s -r -d'R' -p$'\033[6n' _ _ cur_y
 	if [[ $cur_y != 1 ]]; then
-		echo -n $'\033[41m↵\033[m\n'
+		echo $'\001\033[41m↵\033[m\002\n\r'
 	fi
 }
 
-if [[ ! $MC_SID ]]; then
-	PROMPT_COMMAND=(
-		"${PROMPT_COMMAND[@]}"
-		__prompt_command_col1
-	)
-fi
+PROMPT_COMMAND+=(__lastexit_save_ps1)
+function __lastexit_save_ps1 { __lastexit_ps1=$?; }
+
+PS1='$(__col1_ps1)\[\033[48;5;053m\033[01;$(( __lastexit_ps1 ? 31 : 32 ))m\][\u'${__docker_ps1}'@\h\[\033[01;36m\]$(__git_ps1 " ($(git repo-name):%s)")\[\033[01;33m\] \W]\$\[\033[00m\] '
+
+unset __docker_ps1
 
 ## If this is an xterm set the title to user@host:dir
 #case $TERM in
