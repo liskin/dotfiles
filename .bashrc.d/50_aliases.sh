fb71ed9587f29ef8ad341b9ddd8c9536175d24ab
Tomas Janousek <tomi@nomi.cz>
2020-12-08 12:15:10 +0000

    bash: Add various google-chrome-app-* aliases

--- .bashrc.d/50_aliases.sh
+++ .bashrc.d/50_aliases.sh
@@ -43,3 +43,10 @@ export-alias mutt-news-kernel='neomutt -g nntp.lore.kernel.org'
 export-alias wiki="vim ~/taskwiki/index.wiki"
 
 export-alias livereload-make="python3 -c 'from livereload import Server, shell; server = Server(); server.watch(\".\", shell(\"make\")); server.serve();'"
+
+export-alias google-chrome-app-diagrams="gtk-launch chrome-pebppomjfocnoigkeepgbmcifnnlndla-Default"
+export-alias google-chrome-app-fb-messenger="google-chrome --app=https://www.messenger.com/"
+export-alias google-chrome-app-keep="google-chrome --app=https://keep.google.com/"
+export-alias google-chrome-app-matrix-element="google-chrome --app=https://app.element.io/"
+export-alias google-chrome-app-slack-altworx="google-chrome --app=https://altworx.slack.com/"
+export-alias google-chrome-app-slack-goodalumni="google-chrome --app=https://goodalumni.slack.com/"
