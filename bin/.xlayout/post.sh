77a095168cbe3d4ebe0be0eb214be6a02bbafda9
Tomas Janousek <tomi@nomi.cz>
2021-01-25 16:30:58 +0000

    bin: Fix touchscreen enabling during multihead

--- bin/.xlayout/post.sh
+++ bin/.xlayout/post.sh
@@ -2,9 +2,5 @@
 
 set -eu
 
-. ~/bin/xrandr-smart
-
 fbsetroot -mod 5 5 -fg rgb:00/10/00 -bg rgb:00/00/00
-
-out_eDP=$(find-xrandr-connected-output 'eDP-*') \
-&& xinput map-to-output 'Raydium Corporation Raydium Touch System' "$out_eDP" 2>/dev/null
+liskin-touchscreen-post
