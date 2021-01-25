299cc5227cea803d519d9e314a8947bef3fab9ac
Tomas Janousek <tomi@nomi.cz>
2021-01-25 16:31:15 +0000

    bash: Add some aliases

--- .bashrc.d/50_aliases.sh
+++ .bashrc.d/50_aliases.sh
@@ -65,6 +65,8 @@ export-alias google-chrome-app-keep="google-chrome --app=https://keep.google.com
 export-alias google-chrome-app-matrix-element="google-chrome --app=https://app.element.io/"
 export-alias google-chrome-app-slack-altworx="google-chrome --app=https://altworx.slack.com/"
 export-alias google-chrome-app-slack-goodalumni="google-chrome --app=https://goodalumni.slack.com/"
+export-alias google-chrome-app-slack-reprisma="google-chrome --app=https://reprisma.slack.com/"
+export-alias google-chrome-app-wire="google-chrome --app=https://app.wire.com/"
 
 export-alias gitg='LC_MESSAGES=C /usr/bin/gitg'
 export-alias meld='LC_MESSAGES=C /usr/bin/meld'
