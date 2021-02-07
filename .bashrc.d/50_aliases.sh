71627f99c7fc3ccb44363289c1d436eb348388d2
Tomas Janousek <tomi@nomi.cz>
2021-02-07 13:37:09 +0000

    bash: Add alias for Skype

--- .bashrc.d/50_aliases.sh
+++ .bashrc.d/50_aliases.sh
@@ -68,6 +68,7 @@ export-alias google-chrome-app-slack-goodalumni="google-chrome --app=https://goo
 export-alias google-chrome-app-slack-reprisma="google-chrome --app=https://reprisma.slack.com/"
 export-alias google-chrome-app-wire="google-chrome --app=https://app.wire.com/"
 export-alias google-chrome-app-discord="google-chrome --app=https://discord.com/app"
+export-alias google-chrome-app-skype="google-chrome --app=https://web.skype.com/"
 
 export-alias gitg='LC_MESSAGES=C /usr/bin/gitg'
 export-alias meld='LC_MESSAGES=C /usr/bin/meld'
