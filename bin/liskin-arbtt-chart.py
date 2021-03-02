16c15a4ec86dfdb84db646e657c18e002a63b7a2
Tomas Janousek <tomi@nomi.cz>
2021-03-02 00:32:48 +0000

    arbtt: Separate charts for multiple categories

--- bin/liskin-arbtt-chart.py
+++ bin/liskin-arbtt-chart.py
@@ -1,9 +1,11 @@
 #!/usr/bin/env python3
 
+from collections import namedtuple
 from functools import reduce
 from io import StringIO
 from shutil import get_terminal_size
 from sys import stdin
+
 import numpy as np
 import pandas as pd
 
@@ -27,34 +29,101 @@ def read_csv(inp):
     )
 
 
-def preprocess(tables, stacked):
-    table = reduce(lambda a, b: a.add(b, fill_value=0), tables)
-    table = table.set_index(pd.MultiIndex.from_frame(
-        table.index.str.extract(r'^(\(.*\)$|[A-Za-z]+)-?(.*)'),
-        names=['Category', 'Detail']))
-    table['TimeCategory'] = table.groupby(level='Category').transform('sum')
-    table.sort_values(['TimeCategory', 'Time'], ascending=False, inplace=True)
-    table_noscreen = table.drop(('(screen)', ''))
-    table = table_noscreen.append(table.loc[('(screen)', '')])
-    total = table_noscreen['Time'].sum()
-    if pd.isna(total) or total.total_seconds() == 0:
-        return None, None
-    table['Frac'] = table['Time'] / total
+def has_meaningful_data(table):
+    total = table['Time'].sum()
+    return not pd.isna(total) and total.total_seconds() > 0
+
+
+def extract_category(table):
+    [category] = table.index.str.extract(r'^([\w-]+):', expand=False).dropna().unique()
+    return category, table.rename(lambda s: s.removeprefix(category + ':'))
+
+
+def load_inputs():
+    inputs = {}
+    for csv in read_blank_separated_stdin():
+        table = read_csv(csv)
+        if not has_meaningful_data(table):
+            continue
+
+        category, table = extract_category(table)
+        if category in inputs:
+            inputs[category] = inputs[category].add(table, fill_value=0)
+        else:
+            inputs[category] = table
+
+    return inputs
+
+
+Preprocessed = namedtuple('Preprocessed', 'category time_sum table table_totals')
+
+
+def preprocess(category, table, subtags=True, totals_re=r'^\(screen\)'):
+    if subtags:
+        table = table.set_index(pd.MultiIndex.from_frame(
+            table.index.str.extract(r'^(\(.*\)$|\w*)-?(.*)'),
+            names=['Tag', 'SubTag']))
+
+    # sort
+    table['TimeTag'] = table.groupby(level='Tag').transform('sum')
+    table.sort_values(['TimeTag', 'Time'], ascending=False, inplace=True)
+    table.drop('TimeTag', axis='columns', inplace=True)
+
+    # separate total(-ish) rows
+    totals_m = table.index.get_level_values('Tag').str.contains(totals_re)
+    table, table_totals = table[~totals_m], table[totals_m]
+
+    time_sum = table['Time'].sum()
+    return Preprocessed(category=category, time_sum=time_sum, table=table, table_totals=table_totals)
+
+
+def blank(df):
+    some_name = df.index[0]
+    if type(some_name) == tuple:
+        name = tuple('' for _ in some_name)
+    elif type(some_name) == str:
+        name = ''
+    else:
+        raise Exception(f"blank: unexpected type ({type(some_name)})")
+
+    index = df.index.reindex([name])[0]
+    return pd.DataFrame({'Time': [''], 'Type': ['text']}, index=index)
+
+
+def prepare_bartable(time_total, prep, stacked=True):
+    table = prep.table.assign(Frac=prep.table['Time'] / time_total)
     table['FracAbove'] = table['Frac'].shift(1, fill_value=0).cumsum() if stacked else 0
-    table.loc[('(screen)', ''), 'FracAbove'] = 0
-    hour_frac = pd.to_timedelta('01:00:00') / total
-    return table, hour_frac
+    table['Time'] = table['Time'].map(fmt_time)
+    table['Type'] = 'bar'
+
+    table_totals = prep.table_totals.assign(Frac=prep.table_totals['Time'] / time_total)
+    table_totals['FracAbove'] = 0
+    table_totals['Time'] = table_totals['Time'].map(fmt_time)
+    table_totals['Type'] = 'total_bar'
+
+    blank_line = blank(table)
+    header_line1 = blank_line.rename(lambda _: prep.category, level='Tag')
+    header_line2 = header_line1.rename(lambda s: f"{'═' * len(s)}", level='Tag')
+
+    if len(table_totals):
+        table_totals = pd.concat([blank_line, table_totals])
+
+    return pd.concat([header_line1, header_line2, table, table_totals])
 
 
-def output_table(width, table, hour_frac):
-    time_col = table['Time'].map(lambda x: strfdelta(x, "{hours:02}:{minutes:02}:{seconds:02}"))
-    width -= table.index.levels[0].str.len().max() + 1 + table.index.levels[1].str.len().max() + 2
+def draw_bartable(width, hour_frac, table):
+    for level in table.index.names:
+        width -= table.index.get_level_values(level).str.len().max() + 1
+    width -= 1
+
+    time_col = table['Time']
     width -= time_col.str.len().max() + 2
-    bar_col = table.apply(lambda r: bar(width, r.FracAbove, r.Frac, hour_frac), axis=1)
+
+    bar_col = table.apply(lambda r: bar(width, hour_frac, r), axis=1)
+
     out = pd.DataFrame({'Time': time_col, '': bar_col})
-    out.index.names = [None, None]
-    blank = pd.DataFrame({'Time': '', '': ''}, index=[('', '')])
-    return pd.concat([out.iloc[:-1], blank, out.iloc[-1:]])
+    out.index.names = [None for _ in out.index.names]
+    return out
 
 
 def setup_width():
@@ -62,6 +131,10 @@ def setup_width():
     pd.set_option('display.width', width)
     pd.set_option('max_colwidth', width)
     return width
+
+
+def fmt_time(td):
+    return strfdelta(td, "{hours:02}:{minutes:02}:{seconds:02}")
 
 
 def strfdelta(tdelta, fmt):
@@ -73,7 +146,13 @@ def strfdelta(tdelta, fmt):
     return fmt.format(**d)
 
 
-def bar(width, left_pad_frac, bar_frac, hour_frac):
+def bar(width, hour_frac, r):
+    if r.Type == 'text':
+        return ''
+
+    left_pad_frac = r.FracAbove
+    bar_frac = r.Frac
+
     # characters
     bar_char_pad = "·"
     bar_chars_left = "▏▎▍▌▋▊▉█"
@@ -125,9 +204,22 @@ def bar(width, left_pad_frac, bar_frac, hour_frac):
     return bar
 
 
-inputs = map(read_csv, read_blank_separated_stdin())
-table, hour_frac = preprocess(inputs, stacked=True)
-if table is None:
-    exit()
-output = output_table(setup_width(), table, hour_frac)
-print(output.to_string(header=False))
+def bartable(inputs):
+    if not inputs:
+        return '(no meaningful inputs)'
+
+    preprocessed = [preprocess(category, table) for category, table in inputs.items()]
+    time_total = max(prep.time_sum for prep in preprocessed)
+    hour_frac = pd.to_timedelta('01:00:00') / time_total
+
+    table = reduce(
+        lambda a, b: pd.concat([a, blank(a), b]),
+        (prepare_bartable(time_total, prep) for prep in preprocessed)
+    )
+
+    width = setup_width()
+    output = draw_bartable(width=width, hour_frac=hour_frac, table=table)
+    return output.to_string(header=False)
+
+
+print(bartable(load_inputs()))
