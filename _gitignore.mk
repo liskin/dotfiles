6f67a5ea047ddab29824653e8267fde969239e54
Tomas Janousek <tomi@nomi.cz>
2021-03-14 10:10:41 +0000

    _gitignore.mk: More robust collection of targets to ignore

--- _gitignore.mk
+++ _gitignore.mk
@@ -1,9 +1,5 @@
 all: .gitignore
-.gitignore: Makefile
-	grep -P -o '^all: \K.*' $< \
-		| xargs -n1 \
-		| { grep -v -F -e / || :; } \
-		| { grep -v -F -x -e .gitignore || :; } \
-		| sed 's|^|/|' \
-		>> $@
+.SKIP_GITIGNORE: .gitignore
+.gitignore: Makefile ~/_gitignore.mk
+	liskin-make-gitignore >>$@
 	LC_ALL=C sort -u -o $@ $@
