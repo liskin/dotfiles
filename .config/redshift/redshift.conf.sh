2da5dcb7597f6a2987ab5751643bd4f54d85d3ed
Tomas Janousek <tomi@nomi.cz>
2021-05-05 17:49:43 +0100

    bin: Add liskin-timezone-auto
    Useless until we get to travel again. :-/

--- .config/redshift/redshift.conf.sh
+++ .config/redshift/redshift.conf.sh
@@ -2,7 +2,10 @@
 
 set -eu
 
-location=$(curl --silent --show-error --fail --ipv4 "https://location.services.mozilla.com/v1/geolocate?key=geoclue")
-lat=$(jq -r .location.lat <<<"$location")
-lon=$(jq -r .location.lng <<<"$location")
-m4 -DLAT="$lat" -DLON="$lon" "$@"
+if [[ -z ${LAT-} || -z ${LON-} ]]; then
+	location=$(curl --silent --show-error --fail --ipv4 "https://location.services.mozilla.com/v1/geolocate?key=geoclue")
+	LAT=$(jq -r .location.lat <<<"$location")
+	LON=$(jq -r .location.lng <<<"$location")
+fi
+
+m4 -DLAT="$LAT" -DLON="$LON" "$@"
