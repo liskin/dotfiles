cfc1500a4c9f2964efc28eadc1bb015b6ee522d3
Tomas Janousek <tomi@nomi.cz>
2021-12-03 14:59:30 +0000

    bash: Add 50_aliases_sudo.sh

---
+++ .bashrc.d/50_aliases_sudo.sh
@@ -0,0 +1,12 @@
+#!bash
+# shellcheck disable=SC2239
+
+alias reboot='sudo reboot'
+alias halt='sudo halt'
+alias poweroff='sudo poweroff'
+
+alias sudo-docker='sudo -g docker --preserve-env newgrp $(id -n -g)'
+alias sudo-libvirt='sudo -g libvirt --preserve-env newgrp $(id -n -g)'
+alias docker-stop='sudo service docker stop && sudo systemctl start docker.socket'
+
+alias liskin-battery-graph='sudo /usr/local/sbin/liskin-battery-graph'
