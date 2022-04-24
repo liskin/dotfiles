aab4f4675e827c2e29ed3478360f0a6a2f3a3d79
Tomas Janousek <tomi@nomi.cz>
2022-04-24 21:51:36 +0100

    bash: Alias rfkill=/sbin/rfkill

--- .bashrc.d/50_aliases.sh
+++ .bashrc.d/50_aliases.sh
@@ -90,3 +90,5 @@ alias arbtt=liskin-arbtt-stats
 
 alias paste-x0at='curl -F "file=@-" https://x0.at/; echo'
 alias paste-sprungeus='curl -F "sprunge=<-" http://sprunge.us; echo'
+
+export-alias rfkill=/sbin/rfkill
