062ff726d52bf1f3c9ade8ba885eebef8a7712f3
Tomas Janousek <tomi@nomi.cz>
2022-05-01 00:00:39 +0100

    xmonad: Use fnNerd in xmobar.hs

--- .xmonad/xmobar.hs
+++ .xmonad/xmobar.hs
@@ -12,8 +12,8 @@ myConfig = do
     hasEth0 <- doesPathExist "/sys/class/net/eth0"
     hasBat1 <- doesPathExist "/sys/class/power_supply/BAT1"
     let netCmds =
-            [ Run $ Network "wlan1" ["-L","0","-H","32768","--normal","green","--high","red","-t","<fn=3>\xfaa8 </fn><rx>KB|<tx>KB "] 10 ] <>
-            [ Run $ Network "eth0" ["-L","0","-H","32768","--normal","green","--high","red","-t","<fn=3>\xf6ff </fn><rx>KB:<tx>KB "] 10 | hasEth0 ]
+            [ Run $ Network "wlan1" ["-L","0","-H","32768","--normal","green","--high","red","-t",fnNerd "\xfaa8" <> "<rx>KB|<tx>KB "] 10 ] <>
+            [ Run $ Network "eth0" ["-L","0","-H","32768","--normal","green","--high","red","-t",fnNerd "\xf6ff" <> "<rx>KB:<tx>KB "] 10 | hasEth0 ]
     let batCmds =
             [ Run $ BatteryN ["BAT0"] ["-L","5","-H","70","--high","green","--low","red","-d","1","-t","<acstatus><left>%|<watts>W","--","-O","<icon=power-ac.xbm/>","-i","<icon=power-ac.xbm/>","-o","<icon=power-bat.xbm/>"] 50 "BAT0" ] <>
             [ Run $ BatteryN ["BAT1"] ["-L","5","-H","70","--high","green","--low","red","-d","1","-t","|<left>%|<watts>W"] 50 "BAT1" | hasBat1 ]
@@ -36,12 +36,12 @@ myConfig = do
             [ "%wlan1%" ] <>
             [ "<fc=white>%theDate%</fc>" ]
         , commands =
-            [ Run $ Date "<fn=3>\xf5ef </fn>%a <action=`date +%F | xclip -r`>%F</action> %H:%M:%S" "theDate" 10
+            [ Run $ Date (fnNerd "\xf5ef" <> "%a <action=`echo %F | xclip -r`>%F</action> %H:%M:%S") "theDate" 10
             , Run $ UnsafeXMonadLog
             , Run $ XPropertyLog "_XMOBAR_BATTERY_EXTRA"
             , Run $ XPropertyLog "_XMOBAR_DND"
             , Run $ XPropertyLog "_XMOBAR_DEVICES"
-            , Run $ CoreTemp ["-t","<fn=3>\xf2c9 </fn><core0>C"] 50
+            , Run $ CoreTemp ["-t",fnNerd "\xf2c9" <> "<core0>C"] 50
             , Run $ MultiCpu ["-H","95","--high","red","-t","<icon=cpu.xbm/><icon=l.xbm/><autovbar><icon=r.xbm/>","--","--contiguous-icons"] 10
             ] <> netCmds <> batCmds
         }
