96e6313f960fbca79f0faf887b63660cbd43a285
Tomas Janousek <tomi@nomi.cz>
2022-05-11 16:38:13 +0100

    bin/.ext-npm: Add infra for global npm install; add redoc-cli

--- .bashrc.d/10_env.sh
+++ .bashrc.d/10_env.sh
@@ -10,6 +10,7 @@ export PATH="$HOME"/bin/.untracked:"$PATH"
 export PATH="$HOME"/bin/.aliases:"$PATH"
 export PATH="$HOME"/bin:"$PATH"
 export PATH="$HOME"/bin/.ext:"$PATH"
+export PATH="$HOME"/bin/.ext-npm:"$PATH"
 export PATH="$HOME"/.local/bin:"$PATH"
 
 export MAIL=/var/mail/tomi
