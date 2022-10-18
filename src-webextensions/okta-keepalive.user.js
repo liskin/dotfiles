liskin/dotfiles

commit 7dcd7dc2389d9e9e093974425841e579d08d8959

src-webextensions/okta: Fix dashboard failing to load

src-webextensions/okta-keepalive.user.js
@@ -6,11 +6,14 @@
 // @version     1
 // ==/UserScript==
 
-/* disable session timeout timers */
-const maxIntervalId = setTimeout(function(){}, 0);
-for (let i = 0; i <= maxIntervalId; ++i) {
-	clearInterval(i);
-}
+/* after the page completes loading */
+setTimeout(function () {
+	/* disable session timeout timers */
+	const maxIntervalId = setTimeout(function(){}, 0);
+	for (let i = 0; i <= maxIntervalId; ++i) {
+		clearInterval(i);
+	}
+}, 60000);
 
 /* refresh session token every 10 minutes */
 setInterval(async function () {

commit 94882b2e5804a054d2829a7df09d1e938340e464

src-webextensions/okta: Fix token refreshing again
Scheduling a timer and then disabling it together with all the other
timers is silly, isn't it… :-)

Fixes: 7dcd7dc2389d ("src-webextensions/okta: Fix dashboard failing to load")

src-webextensions/okta-keepalive.user.js
@@ -13,12 +13,12 @@ setTimeout(function () {
 	for (let i = 0; i <= maxIntervalId; ++i) {
 		clearInterval(i);
 	}
-}, 60000);
 
-/* refresh session token every 10 minutes */
-setInterval(async function () {
-	if (window.navigator.onLine) {
-		document.cookie = `srefresh=${Date.now()}; path=/; secure`;
-		await fetch("/api/v1/sessions/me/lifecycle/refresh", {method: "POST", headers: {"Accept": "application/json"}});
-	}
-}, 600000);
+	/* refresh session token every 10 minutes */
+	setInterval(async function () {
+		if (window.navigator.onLine) {
+			document.cookie = `srefresh=${Date.now()}; path=/; secure`;
+			await fetch("/api/v1/sessions/me/lifecycle/refresh", {method: "POST", headers: {"Accept": "application/json"}});
+		}
+	}, 600000);
+}, 60000);