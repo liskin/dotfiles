3815499149398caa9cbe5d45e7af3ef6bc888041
Tomas Janousek <tomi@nomi.cz>
2022-11-11 17:52:47 +0000

    bin/.ext: Add rustup(-init)

--- .bashrc.d/10_env.sh
+++ .bashrc.d/10_env.sh
@@ -11,6 +11,9 @@ export PATH="$HOME"/bin/.aliases:"$PATH"
 export PATH="$HOME"/bin:"$PATH"
 export PATH="$HOME"/bin/.ext:"$PATH"
 export PATH="$HOME"/bin/.ext-npm:"$PATH"
+if [ -e "$HOME"/bin/.rustup/.prefer-rustup ]; then
+	export PATH="$HOME"/bin/.rustup:"$PATH"
+fi
 export PATH="$HOME"/.local/bin:"$PATH"
 
 export MAIL=/var/mail/tomi
