9550032442304d630ddbf4e58be63306717bf04b
Tomas Janousek <tomi@nomi.cz>
2023-08-06 00:15:07 +0100

    nvim: Use nvim-treesitter for rust

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,1 +1,10 @@
 vim.cmd.source(vim.fn.stdpath("config") .. "/vimrc.vim")
+
+require'nvim-treesitter.configs'.setup {
+	ensure_installed = {
+		"rust",
+	},
+	highlight = {
+		enable = true,
+	},
+}
