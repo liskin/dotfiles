8bf5f2ec61aa8369912a6a6080035d4c53226775
Tomas Janousek <tomi@nomi.cz>
2023-11-12 12:16:16 +0000

    vim: Fix (in a different way) nvim-treesitter not updating parsers
    Upstream seems to recommend this error-prone way instead… 🤦

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -3,7 +3,12 @@ vim.cmd.source(vim.fn.stdpath("config") .. "/vimrc.vim")
 require'nvim-treesitter.configs'.setup {
 	sync_install = true,
 	ensure_installed = {
+		"c",
+		"lua",
+		"query",
 		"rust",
+		"vim",
+		"vimdoc",
 	},
 	highlight = {
 		enable = true,
