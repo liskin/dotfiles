32839d0353453301e97bb2e8ffaebc0e6fc4d30c
Tomas Janousek <tomi@nomi.cz>
2023-11-14 16:30:27 +0000

    vim: Right align virtual text

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -33,9 +33,20 @@ vim.diagnostic.config {
 	virtual_text = {
 		source = 'if_many',
 		spacing = 10,
-		-- virt_text_pos = "right", -- needs nightly neovim
+		-- virt_text_pos = 'right_align', -- needs nightly neovim
 	},
 }
+
+-- hack for right alignment of virtual text
+-- TODO: drop in favor of vim.diagnostic.config when possible
+local orig_vim_api_nvim_buf_set_extmark = vim.api.nvim_buf_set_extmark
+function vim.api.nvim_buf_set_extmark(buffer, ns_id, line, col, opts, ...)
+	opts = opts or {}
+	if col == 0 then
+		opts.virt_text_pos = opts.virt_text_pos or 'right_align'
+	end
+	return orig_vim_api_nvim_buf_set_extmark(buffer, ns_id, line, col, opts, ...)
+end
 
 -- rounded borders of LSP floating windows
 local orig_util_open_floating_preview = vim.lsp.util.open_floating_preview
