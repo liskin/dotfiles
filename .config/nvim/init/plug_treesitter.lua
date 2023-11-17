d2abb4cbc1b30bbb4085a79d966f6707e6031032
Tomas Janousek <tomi@nomi.cz>
2023-11-17 16:38:42 +0000

    vim: Enable treesitter for more languages

--- .config/nvim/init/plug_treesitter.lua
+++ .config/nvim/init/plug_treesitter.lua
@@ -1,9 +1,17 @@
+---@diagnostic disable: missing-fields
 require'nvim-treesitter.configs'.setup {
 	sync_install = true,
 	ensure_installed = {
 		"c",
+		"cpp",
+		"go",
+		"html",
 		"lua",
+		"markdown",
+		"markdown_inline",
+		"python",
 		"query",
+		"regex",
 		"rust",
 		"vim",
 		"vimdoc",
