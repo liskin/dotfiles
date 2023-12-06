4b73815e33f3aa3dfd3cb54c25283c12bc25839b
Tomas Janousek <tomi@nomi.cz>
2023-12-06 00:50:06 +0000

    vim: Add LspLinesToggle command

--- .config/nvim/init/plug_lsp_lines.lua
+++ .config/nvim/init/plug_lsp_lines.lua
@@ -1,6 +1,16 @@
 require'lsp_lines'.setup()
-vim.diagnostic.config {
-	virtual_lines = {
-		only_current_line = true,
-	},
+
+local virtual_lines_config = {
+	only_current_line = true,
 }
+vim.diagnostic.config { virtual_lines = virtual_lines_config }
+
+vim.api.nvim_create_user_command("LspLinesToggle", function()
+	if vim.diagnostic.config().virtual_lines then
+		vim.diagnostic.config { virtual_lines = false }
+	else
+		vim.diagnostic.config { virtual_lines = virtual_lines_config }
+	end
+end, {
+	desc = "Toggle lsp_lines",
+})
