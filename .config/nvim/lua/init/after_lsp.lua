b3444af965e796281b9b7ba1a61da475fd6b8b7c
Tomas Janousek <tomi@nomi.cz>
2024-01-24 09:22:23 +0000

    vim: Bump neodev.nvim

--- .config/nvim/lua/init/after_lsp.lua
+++ .config/nvim/lua/init/after_lsp.lua
@@ -46,26 +46,6 @@ lspconfig.util.on_setup = lspconfig.util.add_hook_after(lspconfig.util.on_setup,
 	end
 
 	if config.name == 'lua_ls' then
-		-- workaround for nvim's incorrect handling of scopes in the workspace/configuration handler
-		-- https://github.com/folke/neodev.nvim/issues/41
-		-- https://github.com/LuaLS/lua-language-server/issues/1089
-		-- https://github.com/LuaLS/lua-language-server/issues/1596
-		config.handlers = vim.tbl_extend('error', {}, config.handlers)
-		config.handlers['workspace/configuration'] = function(...)
-			local _, result, ctx = ...
-			local client_id = ctx.client_id
-			local client = vim.lsp.get_client_by_id(client_id)
-			if client and client.workspace_folders and #client.workspace_folders then
-				if result.items and #result.items > 0 then
-					if not result.items[1].scopeUri then
-						return vim.tbl_map(function(_) return nil end, result.items)
-					end
-				end
-			end
-
-			return vim.lsp.handlers['workspace/configuration'](...)
-		end
-
 		-- resolve symlinks before looking for root_dir
 		local orig_root_dir = config.root_dir
 		config.root_dir = function(fname)
