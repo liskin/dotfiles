97277410f185c34bb2fc5461af05f87008cb114f
Tomas Janousek <tomi@nomi.cz>
2024-06-05 22:47:16 +0100

    make: Use relative deps for .gitignore to fix bootstrapping

--- _gitignore.mk
+++ _gitignore.mk
@@ -1,6 +1,6 @@
 all: .gitignore
 .SKIP_GITIGNORE: .gitignore
-.gitignore: Makefile ~/_gitignore.mk
+.gitignore: Makefile $(shell realpath --relative-to=. ~/_gitignore.mk ~/bin/liskin-make-gitignore)
 	liskin-make-gitignore >>$@
 	LC_ALL=C sort -u -o $@ $@
 .PRECIOUS: .gitignore
