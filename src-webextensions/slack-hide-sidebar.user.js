7327d88ae2eae14fb823bb824751659411c360b0
Tomas Janousek <tomi@nomi.cz>
2024-06-25 17:58:34 +0200

    src-webextensions/slack: Fix pane widths when a thread is open

--- src-webextensions/slack-hide-sidebar.user.js
+++ src-webextensions/slack-hide-sidebar.user.js
@@ -14,12 +14,15 @@ const toggleSidebars = function () {
 
 	const clientWorkspaceWrapper = document.querySelector("div.p-client_workspace_wrapper");
 	const clientWorkspaceLayout = document.querySelector("div.p-client_workspace__layout");
+	const viewContentsPrimary = document.querySelector("div.p-view_contents--primary");
 	const tabRail = document.querySelector("div.p-tab_rail");
 	const controlStrip = document.querySelector("div.p-control_strip");
-	if (!clientWorkspaceWrapper || !clientWorkspaceLayout || !tabRail || !controlStrip) {
+	const resizers = document.querySelectorAll("div.p-resizer");
+	if (!clientWorkspaceWrapper || !clientWorkspaceLayout || !viewContentsPrimary || !tabRail || !controlStrip) {
 		console.warn("slack-hide-sidebar.user.js - can't find needed elements");
 		console.warn(`clientWorkspaceWrapper = ${clientWorkspaceWrapper}`);
 		console.warn(`clientWorkspaceLayout = ${clientWorkspaceLayout}`);
+		console.warn(`viewContentsPrimary = ${viewContentsPrimary}`);
 		console.warn(`tabRail = ${tabRail}`);
 		console.warn(`controlStrip = ${controlStrip}`);
 		return;
@@ -28,13 +31,21 @@ const toggleSidebars = function () {
 	if (hiddenSidebar) {
 		clientWorkspaceWrapper.style.gridTemplateColumns = '0 auto';
 		clientWorkspaceLayout.style.gridTemplateColumns = '0 auto';
+		viewContentsPrimary.style.minWidth = '50vw';
 		tabRail.style.display = 'none';
 		controlStrip.style.display = 'none';
+		for (const resizer of resizers) {
+			resizer.style.display = 'none';
+		}
 	} else {
 		clientWorkspaceWrapper.style.gridTemplateColumns = null;
 		clientWorkspaceLayout.style.gridTemplateColumns = null;
+		viewContentsPrimary.style.minWidth = null;
 		tabRail.style.display = null;
 		controlStrip.style.display = null;
+		for (const resizer of resizers) {
+			resizer.style.display = null;
+		}
 	}
 };
 
