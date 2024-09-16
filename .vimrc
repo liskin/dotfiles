liskin/dotfiles

commit a193c87cd4ae6ea6c52f2d8cbf4a1b0f32e5d15e

vim: Drop some unnecessary Select mode mappings
Yeah Select mode is stupid but I've done it already anyway…

.vimrc
@@ -633,14 +633,14 @@ nnoremap <silent> <C-Q> <C-X>
 vnoremap <silent> <C-Q> <C-X>
 
 " prevent x from overriding what's in the clipboard. {{{2
-noremap <silent> x "_x
-noremap <silent> X "_X
+nnoremap <silent> x "_x
+nnoremap <silent> X "_X
 
 " seamlessly treat visual lines as actual lines when moving around. {{{2
 nnoremap <silent> j gj
 nnoremap <silent> k gk
-vnoremap <silent> j gj
-vnoremap <silent> k gk
+xnoremap <silent> j gj
+xnoremap <silent> k gk
 inoremap <silent> <Down> <C-O>gj
 inoremap <silent> <Up> <C-O>gk
 

commit 615a5ca311d2ec1bcfc618f6b8098599baf9b563

vim: Fix x/X in visual mode
Fixes: a193c87cd4ae ("vim: Drop some unnecessary Select mode mappings")

.vimrc
@@ -651,7 +651,9 @@ vnoremap <silent> <C-Q> <C-X>
 
 " prevent x from overriding what's in the clipboard. {{{2
 nnoremap <silent> x "_x
+vnoremap <silent> x "_x
 nnoremap <silent> X "_X
+vnoremap <silent> X "_X
 
 " seamlessly treat visual lines as actual lines when moving around. {{{2
 nnoremap <silent> j gj