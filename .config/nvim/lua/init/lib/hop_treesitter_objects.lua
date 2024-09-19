065b16df4e215426321966ec91303da81d94a916
Tomas Janousek <tomi@nomi.cz>
2024-09-19 01:33:13 +0100

    vim: Deduplicate, sort text_objects jump targets

--- .config/nvim/lua/init/lib/hop_treesitter_objects.lua
+++ .config/nvim/lua/init/lib/hop_treesitter_objects.lua
@@ -3,40 +3,57 @@ local ts_lib = require'init.lib.treesitter'
 local M = {}
 M.opts = {}
 
-local function text_objects(captures)
+local function text_objects(captures, opts)
 	local objects = ts_lib.text_objects()
 	local jump_targets = {}
+	local seen = {}
 
 	for _, object in ipairs(objects or {}) do
 		if not captures or vim.tbl_contains(captures, object.capture) then
 			for _, node in ipairs(object.nodes) do
 				local row, col = node:range()
-				local jump_target = {
-					window = 0,
-					buffer = 0,
-					cursor = {
-						row = row + 1,
-						col = col,
-					},
-					length = 0,
-				}
-				table.insert(jump_targets, jump_target)
-				-- FIXME: filter out duplicates and invisible targets
+				row = row + 1
+
+				local seen_key = row .. ':' .. col
+				if not seen[seen_key] then
+					seen[seen_key] = true
+
+					table.insert(jump_targets, {
+						window = 0,
+						buffer = 0,
+						cursor = {
+							row = row,
+							col = col,
+						},
+						length = 0,
+					})
+				end
+				-- FIXME: filter out invisible targets
 			end
 		end
 	end
 
+	local indirect_jump_targets = {}
+	local c_row, c_col = unpack(vim.api.nvim_win_get_cursor(0))
+	local cursor = { row = c_row, col = c_col }
+	for i, jump_target in ipairs(jump_targets) do
+		table.insert(indirect_jump_targets, {
+			index = i,
+			score = opts.distance_method(cursor, jump_target.cursor, opts.x_bias),
+		})
+	end
+	require'hop.jump_target'.sort_indirect_jump_targets(indirect_jump_targets, opts)
+
 	return {
 		jump_targets = jump_targets,
+		indirect_jump_targets = indirect_jump_targets,
 	}
 end
 
 function M.text_objects(captures, opts)
-	local hop = require'hop'
-
 	opts = setmetatable(opts or {}, { __index = M.opts })
-	hop.hint_with(function(_opts)
-		return text_objects(captures)
+	require'hop'.hint_with(function(opts2)
+		return text_objects(captures, opts2)
 	end, opts)
 end
 
