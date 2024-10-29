liskin/dotfiles

commit 86bf0f9f146bd354be3e098bdfc6f7fdbc9bf5ce

bash, vim: Tweak blue color in terminals

.vim/colors/liskin.vim
@@ -271,6 +271,10 @@ let s:urxvt =
 	\, "#eeeeee"
 	\]
 
+" ~/.bashrc.d/20_colors.sh
+let s:urxvt[4] = "#5080cd"
+let s:urxvt[12] = "#5080ff"
+
 function! s:urxvt_hi(hl, ...) abort
 	exe "hi " . a:hl . " " . join(a:000)
 
@@ -330,7 +334,7 @@ if &t_Co >= 256 || has('gui_running')
 		HiU CursorLineNr ctermfg=250
 		HiU DiffAdd ctermbg=4
 		HiU DiffChange ctermbg=5
-		HiU DiffDelete ctermbg=6 ctermfg=12
+		HiU DiffDelete ctermbg=6 ctermfg=19
 		HiU Directory ctermfg=159
 		HiU EndOfBuffer ctermfg=238
 		HiU ErrorMsg ctermbg=1

.bashrc.d/20_colors.sh
@@ -1,0 +1,23 @@
+#!sh
+# shellcheck disable=SC2239
+# shellcheck disable=SC1003
+
+# inspired by https://github.com/chriskempson/base16-shell
+
+if [ -n "$TMUX" ]; then
+  # Tell tmux to pass the escape sequences through
+  # (Source: http://permalink.gmane.org/gmane.comp.terminal-emulators.tmux.user/1324)
+  _set_term_color() { printf '\033Ptmux;\033\033]4;%d;rgb:%s\033\033\\\033\\' "$@"; }
+elif [ "${TERM%%[-.]*}" = "screen" ]; then
+  # GNU screen (screen, screen-256color, screen-256color-bce)
+  _set_term_color() { printf '\033P\033]4;%d;rgb:%s\007\033\\' "$@"; }
+elif [ "${TERM%%-*}" = "linux" ]; then
+  _set_term_color() { printf "\e]P%x%s" "$1" "$(echo "$2" | sed 's/\///g')"; }
+else
+  _set_term_color() { printf '\033]4;%d;rgb:%s\033\\' "$@"; }
+fi
+
+_set_term_color 4 50/80/cd
+_set_term_color 12 50/80/ff
+
+unset _set_term_color

commit e6d6192de25853c3290865b65665f83d4dac25c4

vim: Tweak diff colors
Fixes: 86bf0f9f146b ("bash, vim: Tweak blue color in terminals")

.vim/colors/liskin.vim
@@ -332,9 +332,10 @@ if &t_Co >= 256 || has('gui_running')
 		HiU Constant ctermfg=207
 		HiU CursorLine ctermbg=235
 		HiU CursorLineNr ctermfg=250
-		HiU DiffAdd ctermbg=4
-		HiU DiffChange ctermbg=5
-		HiU DiffDelete ctermbg=6 ctermfg=19
+		HiU DiffAdd ctermbg=22
+		HiU DiffChange ctermbg=19
+		HiU DiffText ctermbg=26
+		HiU DiffDelete ctermbg=52 ctermfg=NONE
 		HiU Directory ctermfg=159
 		HiU EndOfBuffer ctermfg=238
 		HiU ErrorMsg ctermbg=1