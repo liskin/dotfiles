77ecdd34dfd6da9ef8ab01e44fa871f749dd3ce8
Tomas Janousek <tomi@nomi.cz>
2024-11-03 01:59:21 +0000

    vim: Tweak copilot.lua config a bit

--- .config/nvim/lua/init/plug_copilot.lua
+++ .config/nvim/lua/init/plug_copilot.lua
@@ -1,8 +1,37 @@
 local copilot = require'copilot'
 
 copilot.setup {
-	copilot_node_command = 'bwrap-node-copilot',
+	copilot_node_command = 'bwrap-node-copilot', -- restricted jail for the proprietary crap
 	suggestion = {
 		enabled = false,
+	},
+	panel = {
+		keymap = {
+			jump_next = "<M-]>",
+			jump_prev = "<M-[>",
+			open = "<M-CR>",
+		},
+	},
+	filetypes = {
+		["*"] = function()
+			if vim.b.lsp_disabled or vim.g.lsp_disabled then
+				return false
+			end
+			if not vim.bo.modifiable or vim.bo.buftype ~= "" then
+				-- https://github.com/zbirenbaum/copilot.lua/pull/333
+				return false
+			end
+
+			local name = vim.api.nvim_buf_get_name(0)
+			local basename = vim.fs.basename(name)
+
+			-- disable for .env files
+			if string.match(basename, '^%.env.*') then
+				return false
+			end
+
+			-- enable for everything else, should be okay considering I trigger this manually
+			return true
+		end,
 	},
 }
