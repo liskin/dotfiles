cc891dda11503fae447f47a5596c067ce18fc49d
Tomas Janousek <tomi@nomi.cz>
2024-11-05 17:54:23 +0000

    vim: Integrate CopilotChat.nvim with nvim-cmp and fzf-lua

--- .config/nvim/lua/init/plug_copilot.lua
+++ .config/nvim/lua/init/plug_copilot.lua
@@ -1,5 +1,8 @@
 local copilot = require'copilot'
 local copilot_chat = require'CopilotChat'
+local copilot_chat_actions = require'CopilotChat.actions'
+local copilot_chat_cmp = require'CopilotChat.integrations.cmp'
+local copilot_chat_fzflua = require'CopilotChat.integrations.fzflua'
 
 copilot.setup {
 	copilot_node_command = 'bwrap-node-copilot', -- restricted jail for the proprietary crap
@@ -35,4 +38,18 @@ copilot.setup {
 
 copilot_chat.setup {
 	model = 'claude-3.5-sonnet',
+	mappings = {
+		complete = {
+			insert = '',
+		},
+	},
 }
+
+copilot_chat_cmp.setup()
+
+vim.keymap.set('n', '<Leader>cch', function()
+	copilot_chat_fzflua.pick(copilot_chat_actions.help_actions())
+end)
+vim.keymap.set('n', '<Leader>ccp', function()
+	copilot_chat_fzflua.pick(copilot_chat_actions.prompt_actions())
+end)
