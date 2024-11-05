a77caefb3bb9ae987b06f3d8d8c2c78b05a194e7
Tomas Janousek <tomi@nomi.cz>
2024-11-05 00:55:43 +0000

    vim: Fixes for copilot.vim

--- .config/nvim/lua/init/plug_copilot.lua
+++ .config/nvim/lua/init/plug_copilot.lua
@@ -17,10 +17,6 @@ copilot.setup {
 			if vim.b.lsp_disabled or vim.g.lsp_disabled then
 				return false
 			end
-			if not vim.bo.modifiable or vim.bo.buftype ~= "" then
-				-- https://github.com/zbirenbaum/copilot.lua/pull/333
-				return false
-			end
 
 			local name = vim.api.nvim_buf_get_name(0)
 			local basename = vim.fs.basename(name)
