bbf6de3cc466e47b915f58108e050f126cce5709
Tomas Janousek <tomi@nomi.cz>
2024-11-07 12:04:46 +0000

    vim: Show message when toggling lsp_lines

--- .config/nvim/lua/init/plug_lsp_lines.lua
+++ .config/nvim/lua/init/plug_lsp_lines.lua
@@ -11,8 +11,10 @@ vim.diagnostic.config { virtual_lines = virtual_lines_config }
 vim.api.nvim_create_user_command("LspLinesToggle", function()
 	if vim.diagnostic.config().virtual_lines then
 		vim.diagnostic.config { virtual_lines = false }
+		vim.notify("LSP virtual_lines diagnostics disabled")
 	else
 		vim.diagnostic.config { virtual_lines = virtual_lines_config }
+		vim.notify("LSP virtual_lines diagnostics enabled")
 	end
 end, {
 	desc = "Toggle lsp_lines",
