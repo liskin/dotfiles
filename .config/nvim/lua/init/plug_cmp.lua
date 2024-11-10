liskin/dotfiles

commit 95cba3e5f791d48bf6e43b1069a316ea5f394ce5

vim: Avoid Select mode after snippet expansion
Fixes: 04c6315d1e42 ("vim: Use vim.snippet for LSP snippets")
Related: d513f9e631bc ("vim: Map (S-)Tab to jump between snippet placeholders")

.config/nvim/lua/init/plug_cmp.lua
@@ -1,4 +1,24 @@
 local cmp = require'cmp'
+local cmp_nvim_ultisnips_source = require'cmp_nvim_ultisnips.source'
+
+-- avoid Select mode after snippet expansion
+vim.keymap.set({ 'n', 'v', 's', 'i' }, '<Plug>(noselectmode)', function()
+	if vim.fn.mode() == 's' then
+		return '<C-G>'
+	else
+		return ''
+	end
+end, { expr = true })
+local function snippet_noselectmode()
+	local keys = vim.api.nvim_replace_termcodes('<Plug>(noselectmode)', true, false, true)
+	vim.api.nvim_feedkeys(keys, '', true)
+end
+---@diagnostic disable-next-line: duplicate-set-field
+function cmp_nvim_ultisnips_source:execute(completion_item, callback)
+	vim.call("UltiSnips#ExpandSnippet")
+	snippet_noselectmode()
+	callback(completion_item)
+end
 
 local function has_words_before()
 	local line, col = unpack(vim.api.nvim_win_get_cursor(0))
@@ -89,6 +109,7 @@ local function cmp_complete_ultisnips()
 			snippet = {
 				expand = function(args)
 					vim.fn["UltiSnips#Anon"](args.body)
+					snippet_noselectmode()
 				end,
 			},
 		},
@@ -168,6 +189,12 @@ cmp.setup {
 				unpack(commit_characters)
 			}
 		end
+	},
+	snippet = {
+		expand = function(args)
+			vim.snippet.expand(args.body)
+			snippet_noselectmode()
+		end,
 	},
 }
 

.gitmodules
@@ -73,8 +73,8 @@
 	url = https://github.com/mg979/vim-visual-multi
 [submodule ".vim/bundle/ultisnips"]
 	path = .vim/bundle/ultisnips
-	# url = https://github.com/SirVer/ultisnips
-	url = https://github.com/liskin/ultisnips
+	url = https://github.com/SirVer/ultisnips
+	# url = https://github.com/liskin/ultisnips
 [submodule ".vim/bundle/vim-snippets"]
 	path = .vim/bundle/vim-snippets
 	url = https://github.com/honza/vim-snippets

commit b7683e906ffabed31d2440d5659ebe22407bfa29

vim: Avoid Select mode when jumping between snippet placeholders
Fixes: 95cba3e5f791 ("vim: Avoid Select mode after snippet expansion")

.config/nvim/lua/init/plug_cmp.lua
@@ -19,6 +19,22 @@ function cmp_nvim_ultisnips_source:execute(completion_item, callback)
 	snippet_noselectmode()
 	callback(completion_item)
 end
+vim.keymap.set({ 'n', 'v' }, '<Plug>(vim_snippet_next_noselectmode)', function()
+	vim.snippet.jump(1)
+	snippet_noselectmode()
+end)
+vim.keymap.set({ 'n', 'v' }, '<Plug>(vim_snippet_prev_noselectmode)', function()
+	vim.snippet.jump(-1)
+	snippet_noselectmode()
+end)
+vim.keymap.set({ 'n', 'v' }, '<Plug>(ultisnip_next_noselectmode)', function()
+	vim.fn['UltiSnips#JumpForwards']()
+	snippet_noselectmode()
+end)
+vim.keymap.set({ 'n', 'v' }, '<Plug>(ultisnip_prev_noselectmode)', function()
+	vim.fn['UltiSnips#JumpBackwards']()
+	snippet_noselectmode()
+end)
 
 local function has_words_before()
 	local line, col = unpack(vim.api.nvim_win_get_cursor(0))
@@ -208,18 +224,18 @@ end)
 -- smart Tab - snippet placeholders if any
 vim.keymap.set({ 'n', 'v' }, '<Tab>', function()
 	if vim.snippet.active({ direction = 1 }) then
-		return '<Cmd>lua vim.snippet.jump(1)<CR>'
+		return '<Plug>(vim_snippet_next_noselectmode)'
 	elseif vim.fn['UltiSnips#CanJumpForwards']() ~= 0 then
-		return '<Cmd>call UltiSnips#JumpForwards()<CR>'
+		return '<Plug>(ultisnip_next_noselectmode)'
 	else
 		return '<Tab>'
 	end
 end, { expr = true })
 vim.keymap.set({ 'n', 'v' }, '<S-Tab>', function()
 	if vim.snippet.active({ direction = -1 }) then
-		return '<Cmd>lua vim.snippet.jump(-1)<CR>'
+		return '<Plug>(vim_snippet_prev_noselectmode)'
 	elseif vim.fn['UltiSnips#CanJumpBackwards']() ~= 0 then
-		return '<Cmd>call UltiSnips#JumpBackwards()<CR>'
+		return '<Plug>(ultisnip_prev_noselectmode)'
 	else
 		return '<S-Tab>'
 	end