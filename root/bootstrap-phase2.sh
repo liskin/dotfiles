13b39e097f052910138ab413503a836fc44a53f5
Tomas Janousek <tomi@nomi.cz>
2024-11-12 01:14:52 +0000

    Add ~/bootstrap-phase2.sh

---
+++ root/bootstrap-phase2.sh
@@ -0,0 +1,24 @@
+#!bash
+
+cmds_required=(
+	bwrap
+	fzf
+	getcap
+	jq
+	setcap
+)
+files_required=(
+	/etc/profile.d/bash_completion.sh
+)
+pkgs_required=(
+	bash-completion
+	bubblewrap
+	fzf
+	jq
+	libcap2-bin
+)
+if ! has-cmds "${cmds_required[@]}" || ! has-files "${files_required[@]}"; then
+	oo "# apt install ${pkgs_required[*]}"
+fi
+
+oo "# bash -l -c make"
