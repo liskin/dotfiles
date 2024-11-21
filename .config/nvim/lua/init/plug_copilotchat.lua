db0809e94d6490a214c4558ac7e073597d825727
Tomas Janousek <tomi@nomi.cz>
2024-11-21 23:40:44 +0000

    vim: Tweak the CommitRicky prompt to actually do something

--- .config/nvim/lua/init/plug_copilotchat.lua
+++ .config/nvim/lua/init/plug_copilotchat.lua
@@ -2,7 +2,6 @@ local copilot_chat = require'CopilotChat'
 local copilot_chat_actions = require'CopilotChat.actions'
 local copilot_chat_fzflua = require'CopilotChat.integrations.fzflua'
 local copilot_chat_prompts = require'CopilotChat.prompts'
-local copilot_chat_config = require'CopilotChat.config'
 
 local base_sys_prompt = vim.iter(vim.split(copilot_chat_prompts.COPILOT_INSTRUCTIONS, "\n")):skip(1):join("\n")
 
@@ -25,7 +24,10 @@ copilot_chat.setup {
 			system_prompt = "Explain it to me like you're a senior level brogrammer who thinks you're way better than you really are, and you're frustrated that you have to explain this to me.\n" .. base_sys_prompt
 		},
 		CommitRicky = {
-			prompt = '> /SysRicky\n' .. copilot_chat_config.prompts.Commit.prompt
+			prompt =
+				'> /SysRicky\n' ..
+				'> #git:staged\n\n' ..
+				"Write commit message for the change with commitizen convention, but remember you're a comedian and make it funny and sarcastic. Make sure the title has maximum 50 characters and message is wrapped at 72 characters. Wrap the whole message in code block with language gitcommit.",
 		},
 		SysRicky = {
 			system_prompt = "You are Ricky Gervais, a British stand-up comedian. You're known for your dark humor and observational comedy. You often use self-deprecating humor and target social issues and taboos. You are liked for your honesty and willingness to push boundaries.\n" .. base_sys_prompt
