97373bf9ba1626bc947bb820faaa9319069cd7c7
Tomas Janousek <tomi@nomi.cz>
2024-12-03 11:28:29 +0000

    src-webextensions/o365: Favicon badge showing number of unread messages in Teams

--- src-webextensions/o365-teams.user.js
+++ src-webextensions/o365-teams.user.js
@@ -5,8 +5,12 @@
 // @grant       none
 // @version     1
 // @require     https://cdn.jsdelivr.net/gh/uzairfarooq/arrive@v2.4.1/minified/arrive.min.js
+// @require     https://cdn.jsdelivr.net/gh/ejci/favico.js/favico.js
 // ==/UserScript==
 
+// ---
+// "safe" (actually just annoying) links
+// ---
 function dropClickEventListeners(el) {
 	const el2 = el.cloneNode(true);
 	el2.addEventListener('click', (e) => e.stopImmediatePropagation());
@@ -20,3 +24,27 @@ document.arrive('a', function(link) {
 
 	dropClickEventListeners(link);
 });
+
+// ---
+// unread badge in tab icon
+// ---
+const headTitle = document.querySelector("head > title");
+function getUnreadCount() {
+	const m = headTitle.text.match(/^\((\d+)\) /);
+	if (m) {
+		return parseInt(m[1]);
+	} else {
+		return 0;
+	}
+}
+
+const favico = new Favico({animation: 'none'});
+function updateBadge() {
+	favico.badge(getUnreadCount());
+};
+
+// update favicon every 10s
+setInterval(updateBadge, 10000);
+
+// whenever Teams changes the title, update immediately
+new MutationObserver(() => setTimeout(updateBadge, 0)).observe(headTitle, {childList: true, characterData: true});
