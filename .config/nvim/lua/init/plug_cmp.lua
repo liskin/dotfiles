liskin/dotfiles

commit aff028ad8119bbfa2fa9e5cd7ecca9638ef1ad00

vim: Ignore mouse in nvim-cmp

.config/nvim/lua/init/plug_cmp.lua
@@ -175,6 +175,7 @@ cmp.setup {
 		['<C-X><C-P>'] = cmp_or(cmp_select_prev, cmp_complete_buffer_current),
 		['<C-X><C-O>'] = cmp_or(cmp_select_next, cmp_complete_omni),
 		['<C-_>'] = cmp_or(cmp_select_next, cmp_complete_ultisnips),
+		['<LeftMouse>'] = function() end,
 	},
 	---@diagnostic disable-next-line: missing-fields
 	confirmation = {

commit 542464bd37b56a5bb341d89692bc872d69146163

vim: Fix left-click in Insert mode
Fixes: aff028ad8119 ("vim: Ignore mouse in nvim-cmp")

.config/nvim/lua/init/plug_cmp.lua
@@ -172,7 +172,7 @@ cmp.setup {
 		['<C-X><C-P>'] = cmp_or(cmp_select_prev, cmp_complete_buffer_current),
 		['<C-X><C-O>'] = cmp_or(cmp_select_next, cmp_complete_omni),
 		['<C-_>'] = cmp_or(cmp_select_next, cmp_complete_ultisnips),
-		['<LeftMouse>'] = function() end,
+		['<LeftMouse>'] = cmp_or(function() end),
 	},
 	---@diagnostic disable-next-line: missing-fields
 	confirmation = {