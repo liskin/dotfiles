liskin/dotfiles

commit 3a86de99b42b296d4664aa9c954649ce4135add9

vim: Sync rg_opts/fd_opts with my ripgreprc

.config/nvim/lua/init/plug_fzf.lua
@@ -19,6 +19,14 @@ fzf_lua.setup {
 		file_icons = false,
 	},
 
+	files = {
+		fd_opts = '--no-require-git ' .. fzf_lua.config.defaults.files.fd_opts,
+	},
+
+	grep = {
+		rg_opts = '--hidden --follow --no-require-git ' .. fzf_lua.config.defaults.grep.rg_opts,
+	},
+
 	lsp = {
 		symbols = {
 			symbol_style = 3,

commit f69da234f6494b98dcb8b47e1211fac28c49f459

vim: Fix rg searching inside .git
Fixes: 3a86de99b42b ("vim: Sync rg_opts/fd_opts with my ripgreprc")

.config/nvim/lua/init/plug_fzf.lua
@@ -24,7 +24,7 @@ fzf_lua.setup {
 	},
 
 	grep = {
-		rg_opts = '--hidden --follow --no-require-git ' .. fzf_lua.config.defaults.grep.rg_opts,
+		rg_opts = '--hidden --follow --glob=!.git --no-require-git ' .. fzf_lua.config.defaults.grep.rg_opts,
 	},
 
 	lsp = {