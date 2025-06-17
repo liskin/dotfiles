1b6fdbc02d83e8e31e6e0d67cdedfe5b48711516
Tomas Janousek <tomi@nomi.cz>
2025-06-17 01:32:17 +0100

    vim: Drop lsp_lines.nvim in favor of built-in virtual_lines (nvim 0.11)

--- .config/nvim/lua/init/diagnostic.lua
+++ .config/nvim/lua/init/diagnostic.lua
@@ -2,3 +2,30 @@ vim.diagnostic.config {
 	severity_sort = true,
 	virtual_text = false,
 }
+
+local virtual_lines_off = {
+	virtual_text = {
+		-- current_line = true,
+		virt_text_pos = 'eol_right_align',
+	},
+	virtual_lines = false,
+}
+local virtual_lines_on = {
+	virtual_text = false,
+	virtual_lines = {
+		current_line = true,
+	},
+}
+vim.diagnostic.config(virtual_lines_off)
+
+vim.api.nvim_create_user_command("LspLinesToggle", function()
+	if vim.diagnostic.config().virtual_lines then
+		vim.diagnostic.config(virtual_lines_off)
+		vim.notify("LSP virtual_lines diagnostics disabled")
+	else
+		vim.diagnostic.config(virtual_lines_on)
+		vim.notify("LSP virtual_lines diagnostics enabled")
+	end
+end, {
+	desc = "Toggle lsp_lines",
+})
