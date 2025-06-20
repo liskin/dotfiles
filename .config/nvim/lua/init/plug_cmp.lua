4a5a2c2e8673ea700755eb35fc46391feb4b8c8f
Tomas Janousek <tomi@nomi.cz>
2025-06-20 17:21:30 +0100

    vim: Disable nvim-0.11's Tab snippet mappings

--- .config/nvim/lua/init/plug_cmp.lua
+++ .config/nvim/lua/init/plug_cmp.lua
@@ -10,6 +10,12 @@ function cmp_nvim_ultisnips_source:execute(completion_item, callback)
 	callback(completion_item)
 end
 
+-- disable nvim 0.11 default vim.snippet.jump mapping in Insert/Select mode
+if vim.fn.has("nvim-0.11") == 1 then
+	vim.keymap.del({ 'i', 's' }, '<Tab>')
+	vim.keymap.del({ 'i', 's' }, '<S-Tab>')
+end
+
 local function has_words_before()
 	local line, col = unpack(vim.api.nvim_win_get_cursor(0))
 	return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
