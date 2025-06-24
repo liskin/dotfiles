83de70af8a7a3bd19ac1f05a87884187bb31935c
Tomas Janousek <tomi@nomi.cz>
2025-06-24 17:11:34 +0100

    vim: Fix taplo autoformat enabling

--- .config/nvim/lua/init/lsp_settings.lua
+++ .config/nvim/lua/init/lsp_settings.lua
@@ -1,7 +1,7 @@
 local git_root = vim.fs.root(vim.uv.cwd() or error(), ".git")
 
 local function has(filename)
-	return git_root and vim.fn.filereadable(vim.fs.joinpath(git_root, filename))
+	return git_root and vim.fn.filereadable(vim.fs.joinpath(git_root, filename)) == 1
 end
 
 if has('.taplo.toml') or has('taplo.toml') then
