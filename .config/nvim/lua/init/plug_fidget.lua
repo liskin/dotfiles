e821dec333baa3f7bf8cb7519b750d7eb2d96151
Tomas Janousek <tomi@nomi.cz>
2025-07-18 02:36:07 +0100

    vim: Tweak fidget (notifications) config

--- .config/nvim/lua/init/plug_fidget.lua
+++ .config/nvim/lua/init/plug_fidget.lua
@@ -5,6 +5,14 @@ require'fidget'.setup {
 				icon_on_left = true,
 			},
 		},
+		view = {
+			stack_upwards = false,
+		},
+		window = {
+			border = "rounded",
+			border_hl = "Normal",
+			winblend = 0,
+		},
 	},
 	progress = {
 		display = {
