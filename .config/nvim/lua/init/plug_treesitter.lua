4ef6f3ea77531e8245ad9c0b20a69e5bc786cfd3
Tomas Janousek <tomi@nomi.cz>
2025-07-20 17:40:08 +0100

    vim: Switch to nvim-treesitter main branch

--- .config/nvim/lua/init/plug_treesitter.lua
+++ .config/nvim/lua/init/plug_treesitter.lua
@@ -1,53 +1,58 @@
----@diagnostic disable: missing-fields
-require'nvim-treesitter.configs'.setup {
-	sync_install = true,
-	ensure_installed = {
-		"bash",
-		"c",
-		"c_sharp",
-		"comment",
-		"cpp",
-		"css",
-		"diff",
-		"dockerfile",
-		"git_config",
-		"go",
-		"graphql",
-		"html",
-		"javascript",
-		"jsdoc",
-		"json",
-		"json5",
-		"jsonc",
-		"lua",
-		"markdown",
-		"markdown_inline",
-		"mermaid",
-		"python",
-		"query",
-		"regex",
-		"rust",
-		"sql",
-		"vim",
-		"vimdoc",
-		"yaml",
-	},
-	highlight = {
-		enable = true,
-		disable = {
-			"bash", -- too buggy (but stays enabled in injections)
-		},
-	},
-	textobjects = {
-		-- FIXME: doesn't work (https://github.com/nvim-treesitter/nvim-treesitter-textobjects/issues/479)
-		-- move = {
-		-- 	enable = true,
-		-- 	goto_next_start = {
-		-- 		["]l"] = "@markup.link.label"
-		-- 	},
-		-- 	goto_previous_start = {
-		-- 		["[l"] = "@markup.link.label"
-		-- 	},
-		-- },
-	},
+local ts = require'nvim-treesitter'
+
+local ensure_installed = {
+	"bash",
+	"c",
+	"c_sharp",
+	"comment",
+	"cpp",
+	"css",
+	"diff",
+	"dockerfile",
+	"git_config",
+	"go",
+	"graphql",
+	"html",
+	"javascript",
+	"jsdoc",
+	"json",
+	"json5",
+	"jsonc",
+	"lua",
+	"markdown",
+	"markdown_inline",
+	"mermaid",
+	"python",
+	"query",
+	"regex",
+	"rust",
+	"sql",
+	"vim",
+	"vimdoc",
+	"yaml",
 }
+local disabled = {
+	-- "bash",
+}
+
+vim.api.nvim_create_user_command("TSUpdateSync", function()
+	ts.install(ensure_installed):wait(300000)
+	ts.update():wait(300000)
+end, {})
+
+vim.api.nvim_create_autocmd("FileType", {
+	callback = function(ev)
+		local bufnr = ev.buf
+		local filetype = vim.api.nvim_get_option_value("filetype", { buf = bufnr })
+
+		if not filetype or filetype == '' then
+			return
+		end
+
+		local parser_name = vim.treesitter.language.get_lang(filetype)
+		local parser_installed = pcall(vim.treesitter.get_parser, bufnr, parser_name)
+		if parser_installed and not vim.list_contains(disabled, parser_name) then
+			vim.treesitter.start(bufnr, parser_name)
+		end
+	end,
+})
