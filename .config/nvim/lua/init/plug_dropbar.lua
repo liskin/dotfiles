d2847816cd425ec98c8b04408b65372511eedc13
Tomas Janousek <tomi@nomi.cz>
2025-07-20 16:10:38 +0100

    vim: Disable dropbar.nvim on nvim-0.10

--- .config/nvim/lua/init/plug_dropbar.lua
+++ .config/nvim/lua/init/plug_dropbar.lua
@@ -1,3 +1,8 @@
+-- XXX: nvim-0.11 only
+if vim.fn.has('nvim-0.11') == 0 then
+	return
+end
+
 local dropbar = require'dropbar'
 local dropbar_configs = require'dropbar.configs'
 local dropbar_utils = require'dropbar.utils'
