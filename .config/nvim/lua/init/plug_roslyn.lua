74add3c979096caf94da880fb0b5759ecc18a670
Tomas Janousek <tomi@nomi.cz>
2025-07-23 00:43:01 +0200

    vim: Enable inlay hints, disable refs code lens for roslyn

--- .config/nvim/lua/init/plug_roslyn.lua
+++ .config/nvim/lua/init/plug_roslyn.lua
@@ -36,6 +36,26 @@ vim.lsp.config('roslyn', {
 	-- roslyn.nvim assumes mason, reset cmd
 	cmd = vim.lsp.config.roslyn_ls.cmd,
 
+	settings = {
+		['csharp|inlay_hints'] = {
+			csharp_enable_inlay_hints_for_implicit_object_creation = true,
+			csharp_enable_inlay_hints_for_implicit_variable_types = true,
+			csharp_enable_inlay_hints_for_lambda_parameter_types = true,
+			csharp_enable_inlay_hints_for_types = true,
+			dotnet_enable_inlay_hints_for_indexer_parameters = true,
+			dotnet_enable_inlay_hints_for_literal_parameters = true,
+			dotnet_enable_inlay_hints_for_object_creation_parameters = true,
+			dotnet_enable_inlay_hints_for_other_parameters = true,
+			dotnet_enable_inlay_hints_for_parameters = true,
+			dotnet_suppress_inlay_hints_for_parameters_that_differ_only_by_suffix = true,
+			dotnet_suppress_inlay_hints_for_parameters_that_match_argument_name = true,
+			dotnet_suppress_inlay_hints_for_parameters_that_match_method_intent = true,
+		},
+		['csharp|code_lens'] = {
+			dotnet_enable_references_code_lens = false,
+		},
+	},
+
 	commands = {
 		["dotnet.test.run"] = function(command, ctx)
 			local client = vim.lsp.get_client_by_id(ctx.client_id)
