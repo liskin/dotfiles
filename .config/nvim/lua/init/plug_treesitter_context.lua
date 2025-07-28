8c8ba17a2a265b839cb9bd70f42f86149d3dd341
Tomas Janousek <tomi@nomi.cz>
2025-07-28 16:59:09 +0200

    vim: Bind [c to go_to_context

--- .config/nvim/lua/init/plug_treesitter_context.lua
+++ .config/nvim/lua/init/plug_treesitter_context.lua
@@ -1,3 +1,9 @@
-require'treesitter-context'.setup {
+local tc = require"treesitter-context"
+
+tc.setup {
 	enable = true,
 }
+
+vim.keymap.set("n", "[c", function()
+	tc.go_to_context(vim.v.count1)
+end, { silent = true })
