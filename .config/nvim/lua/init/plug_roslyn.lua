6a3a0a51cf68416ad269fe4bdcb0bf2395618a61
Tomas Janousek <tomi@nomi.cz>
2025-08-18 20:35:16 +0100

    vim: Progress notifications for Roslyn dotnet restore

--- .config/nvim/lua/init/plug_roslyn.lua
+++ .config/nvim/lua/init/plug_roslyn.lua
@@ -32,6 +32,40 @@ local function run_tests_handler(err, result, ctx)
 	end
 end
 
+local progress_handles = {}
+vim.api.nvim_create_autocmd("User", {
+	pattern = "RoslynRestoreProgress",
+	callback = function(ev)
+		local client = vim.lsp.get_client_by_id(assert(ev.data.client_id))
+		local token = assert(ev.data.params[1])
+		local handle = progress_handles[token]
+		if handle then
+			handle:report({
+				title = ev.data.params[2].state,
+				message = ev.data.params[2].message,
+			})
+		else
+			progress_handles[token] = fidget.progress.handle.create({
+				title = ev.data.params[2].state,
+				message = ev.data.params[2].message,
+				lsp_client = client,
+			})
+		end
+	end,
+})
+vim.api.nvim_create_autocmd("User", {
+	pattern = "RoslynRestoreResult",
+	callback = function(ev)
+		local handle = progress_handles[ev.data.token]
+		progress_handles[ev.data.token] = nil
+
+		if handle then
+			handle.message = ev.data.err and ev.data.err.message or "Restore completed"
+			handle:finish()
+		end
+	end,
+})
+
 vim.lsp.config('roslyn', {
 	settings = {
 		['csharp|inlay_hints'] = {
