b3e4428afa9714ab63c4c31ea15b7a2992cd46f8
Tomas Janousek <tomi@nomi.cz>
2025-09-26 12:31:44 +0100

    vim: Update lazydev.nvim

--- .config/nvim/lua/init/plug_lazydev.lua
+++ .config/nvim/lua/init/plug_lazydev.lua
@@ -14,25 +14,30 @@ if vim.fn.has('nvim-0.11') == 0 then
 	return
 end
 
-vim.lsp.config('lua_ls', {
-	root_dir = function(bufnr, on_dir)
-		-- attach to existing workspace if possible
-		local ws = lazydev.find_workspace(bufnr)
-		if ws ~= nil then
-			return on_dir(ws)
-		end
+local function root_dir(bufnr, on_dir)
+	vim.print("ahoj")
 
-		-- use ~/.config/nvim for everything nvim-related as that seems to work best
-		local buf_name = vim.api.nvim_buf_get_name(bufnr)
-		if
-			vim.fs.relpath(vim.fn.stdpath("config"), buf_name)
-			or vim.fs.relpath(vim.fn.stdpath("data"), buf_name)
-			or vim.fs.relpath(vim.env.VIMRUNTIME, buf_name)
-		then
-			return on_dir(vim.fn.stdpath("config"))
-		end
+	-- attach to existing workspace if possible
+	local ws = lazydev.find_workspace(bufnr)
+	if ws ~= nil then
+		return on_dir(ws)
+	end
 
-		-- fallback to default (luarc, git, …)
-		return on_dir(nil)
-	end,
-})
+	-- use ~/.config/nvim for everything nvim-related as that seems to work best
+	local buf_name = vim.api.nvim_buf_get_name(bufnr)
+	if
+		vim.fs.relpath(vim.fn.stdpath("config"), buf_name)
+		or vim.fs.relpath(vim.fn.stdpath("data"), buf_name)
+		or vim.fs.relpath(vim.env.VIMRUNTIME, buf_name)
+	then
+		return on_dir(vim.fn.stdpath("config"))
+	end
+
+	-- fallback to default (luarc, git, …)
+	return on_dir(nil)
+end
+
+vim.lsp.config('lua_ls', { root_dir = root_dir })
+vim.schedule(function()
+	vim.lsp.config('lua_ls', { root_dir = root_dir })
+end)
