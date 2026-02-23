a572c2d52b8246f9d93e3c2acf2131df88e8c4b8
Tomas Janousek <tomi@nomi.cz>
2026-02-23 11:54:09 +0000

    vim: Update some plugins

--- .config/nvim/lua/init/plug_zenmode.lua
+++ .config/nvim/lua/init/plug_zenmode.lua
@@ -24,12 +24,6 @@ zen.setup {
 vim.keymap.set('n', '<Leader>zz', function() zen.toggle() end, { desc = "zen-mode toggle" })
 
 ---@diagnostic disable-next-line: duplicate-set-field
-require"zen-mode.plugins".diagnostics = function(_state, disable)
-	-- fix deprecation warnings
-	vim.diagnostic.enable(not disable)
-end
-
----@diagnostic disable-next-line: duplicate-set-field
 require"zen-mode.view".on_win_enter = function()
 	-- don't close on WinEnter into another window,
 	-- it's annoying and sometimes segfaults nvim
