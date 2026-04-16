499c667d769a1b37ec2ef6509e50825de166acf2
Tomas Janousek <tomi@nomi.cz>
2026-04-16 16:27:49 +0100

    bash, bin: Add env vars and config for less, bat, fzf, ripgrep

---
+++ root/.bashrc.d/10_env.sh
@@ -0,0 +1,10 @@
+#!  sh
+# shellcheck disable=SC2239
+
+export BAT_STYLE=plain
+export FZF_DEFAULT_COMMAND='rg --files'
+export FZF_DEFAULT_OPTS='--preview-window=border-left --info=inline-right'
+export FZF_TMUX=0
+export FZF_TMUX_HEIGHT=100%
+export LESS="--quit-if-one-screen --no-init --RAW-CONTROL-CHARS --mouse --wheel-lines=3 --LONG-PROMPT"
+export RIPGREP_CONFIG_PATH=~/.config/ripgreprc
