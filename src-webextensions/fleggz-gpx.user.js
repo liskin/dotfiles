8e3b428d97716c9d3e01db5da0a7fc8f8aff0bd1
Tomas Janousek <tomi@nomi.cz>
2026-04-22 13:29:12 +0100

    src-webextensions/fleggz-gps: Adapt to current Fleggz

--- src-webextensions/fleggz-gpx.user.js
+++ src-webextensions/fleggz-gpx.user.js
@@ -7,6 +7,26 @@
 // @require     https://cdn.jsdelivr.net/npm/file-saver@2.0.5/dist/FileSaver.min.js
 // ==/UserScript==
 
+function decode_coordinates(e) {
+	try {
+		const t = [6, 5, 9], i = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "-", ".", ",", "[", "]"];
+		let r = e.toLowerCase(), n = "";
+		const o = t.length, a = i.length;
+		let s = [];
+		for (let e = 0; e < r.length; e++)
+			s.push(r.slice(e, 1));
+		return Array.from(r).forEach((e, r) => {
+			let s = i.indexOf(e);
+			if (-1 !== s) {
+				let e = (s - t[r % o] + a) % a;
+				n += i[e]
+			}
+		}), n;
+	} catch (error) {
+		return ""
+	}
+}
+
 function doc_gpx() {
 	const doc = document.implementation.createDocument('http://www.topografix.com/GPX/1/1', 'gpx', null);
 	const gpx = doc.children[0];
@@ -38,17 +58,29 @@ function save_gpx(data, filename) {
 	saveAs(blob, filename);
 }
 
-function download() {
-	const fleggz_tracks = FleggzData.getInstance().getCompoundObjects();
-	const fleggz_name = document.querySelectorAll('h3')[0]?.textContent.trim() ?? "Fleggz route";
+async function download_gpx(refno) {
+	const response = await fetch(`https://account.fleggz.com/r_event/${refno}`);
+	if (!response.ok)
+		throw new Error(`fetch of ${refno} failed`);
+
+	const event = await response.json();
+
+	const fleggz_name = `${event.e.start_date} - ${event.e.name}`;
+	const fleggz_tracks = event.s.flatMap(t =>
+		t.type == 'R' ? [{name: t.name, coordinates: JSON.parse(decode_coordinates(t.coordinates))}] : []
+	);
 
 	const gpx = doc_gpx();
 	gpx.setAttribute('version', '1.1');
 	gpx.setAttribute('creator', 'fleggz-gpx');
 
+	const metadata = el_gpx('metadata');
+	metadata.appendChild(el_gpx_text('name', fleggz_name));
+	gpx.appendChild(metadata);
+
 	for (const track of fleggz_tracks) {
-		const track_name = track[1];
-		const track_start = track[5][0];
+		const track_name = track.name;
+		const track_start = track.coordinates[0];
 
 		const wpt = el_gpx('wpt');
 		wpt.setAttribute('lat', track_start[1]);
@@ -63,7 +95,7 @@ function download() {
 	trk.appendChild(el_gpx_text('name', fleggz_name));
 	const trkseg = el_gpx('trkseg');
 	for (const track of fleggz_tracks) {
-		for (const point of track[5]) {
+		for (const point of track.coordinates) {
 			const trkpt = el_gpx('trkpt');
 			trkpt.setAttribute('lat', point[1]);
 			trkpt.setAttribute('lon', point[0]);
@@ -76,4 +108,11 @@ function download() {
 	save_gpx(xml(gpx), 'fleggz_route.gpx');
 }
 
-GM.registerMenuCommand('Download GPX', download);
+function download() {
+	const refno = document.querySelector('a[href*="/gl/"]')?.href?.match(/^https:\/\/account\.fleggz\.com\/gl\/(.*)$/)?.[1];
+	if (refno)
+		download_gpx(refno);
+}
+
+if (typeof GM !== 'undefined')
+	GM.registerMenuCommand('Download GPX', download);
